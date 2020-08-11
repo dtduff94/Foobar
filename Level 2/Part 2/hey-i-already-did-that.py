9aa414735829abfdefcd149f1eb2b2bc1369b395
Daniel Duff <30853711+dtduff94@users.noreply.github.com>
2020-08-11 15:03:34 -0400

    Update hey-i-already-did-that.py

--- Level 2/Part 2/hey-i-already-did-that.py
+++ Level 2/Part 2/hey-i-already-did-that.py
@@ -1,7 +1,7 @@
 def decimalToBase(decimalNumber, b):
     number = int(decimalNumber)
     ints = []
-    while number >= n:
+    while number >= b:
         r = number % b
         ints.append(str(r))
         number = (number - r) // b
@@ -16,3 +16,20 @@ def baseToDecimal(baseNumber, b):
     for index, num in enumerate(ints):
         decimalNumber += int(num) * (b ** index)
     return str(decimalNumber)
+
+def solution(n, b):
+    k = len(n)
+    m = n
+    usedIDS = []
+    while m not in usedIDS:
+        usedIDS.append(m)
+        sortedInts = sorted(m)
+        ascendingInts = ''.join(sortedInts)
+        sortedInts.reverse() 
+        descendingInts = ''.join(sortedInts)
+             
+        subtractValue = int(baseToDecimal(descendingInts, b)) - int(baseToDecimal(ascendingInts, b))
+        m = decimalToBase(str(subtractValue), b)
+        
+        m = (k - len(m)) * '0' + m
+    return len(usedIDS) - usedIDS.index(m)
